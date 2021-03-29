a77211e5ee97b929dc2d0c08afe777329a3aea10
Ryuzaki13 <ryuzaki_dk_rm_guitar@mail.ru>
2021-03-29 10:41:50 +0500

    second commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack<int> queue = new Stack<int>();
+
+			for (int i = 0; i < 5; i++)
+			{
+				queue.Push(i + 1);
+			}
+
+			Console.WriteLine(queue.Peek());
+			Console.WriteLine(queue.Pop());
+			Console.WriteLine(queue.Pop());
+			Console.WriteLine(queue.Pop());
+
+
+
+
+
+
+		}
+	}
+}
